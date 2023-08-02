98490636faed6319dbe9f3568c3e0074c78c7172
Amita Ghale <amitaghale@gmail.com>
2023-08-02 15:19:56 +0100

    :sparkles: Update the main using flask

--- exercise_1.py
+++ exercise_1.py
@@ -1,6 +1,7 @@
 from utilities import get_data
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
 
 if __name__ == '__main__':
-    test = get_data()
-    print(test)
+    app.run()
